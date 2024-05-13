a5e97350987bbbb66be4d5e83529635a9100c65a
Gabriel Wolf <gwolf@student.42vienna.com>
2024-05-13 13:29:54 +0200

    fix: force if wp install failed

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -15,20 +15,26 @@ wp-php_note "Running wp-php docker-entrypoint"
 
 # Check if container is set up
 if ! wp core is-installed 2>/dev/null; then
-	wp-php_note "Wordpress is not installed. Let's try installing it."
-	wp core download
+	wp-php_note "Wordpress is not installed."
+	wp-php_note "Downloading wordpress"
+	wp core download --force
+
+	wp-php_note "Create config"
 	MARIADB_WPUSER_PASSWORD="$(< $MARIADB_WPUSER_PASSWORD_FILE)"
-	wp config create \
+	wp config create --force \
 		--dbname=${WORDPRESS_DB_NAME} \
 		--dbuser=${WORDPRESS_DB_USER} \
 		--dbpass=${MARIADB_WPUSER_PASSWORD} \
 		--dbhost=${MARIADB_HOST}
+
+	wp-php_note "Install wordpress"
 	wp core install \
 		--url=localhost \
 		--title=inception \
 		--admin_user=admin \
 		--admin_password=admin \
 		--admin_email=admin@admin.com
+
 	wp theme install neve --activate
 else
 	wp-php_note "Wordpress is installed."
