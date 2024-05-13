6489575251c543a0bcd608cafc69575af390d728
Gabriel Wolf <gwolf@student.42vienna.com>
2024-05-13 09:31:38 +0200

    feat: only grant certain priv to wpuser

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -23,47 +23,40 @@ mariadb_error() {
 mariadb_note "Entrypoint of mariadb started"
 
 
-# check if database has been setup
-if [ ! -d /var/lib/mysql/wordpress ]; then
+# check if WORDPRESS_DB_NAME has been setup
+if [ ! -d /var/lib/mysql/$WORDPRESS_DB_NAME ]; then
 	mariadb_note "Container mariadb is not set up, setting up now"
 	mariadbd --skip-networking &
 	MARIADB_PID=$!
 	mariadb_note "Wait for server startup"
 	for i in {30..0}; do
-		if mariadb -u root <<<'SELECT 1' &> /dev/null; then
+		if mariadb --database=mysql <<<'SELECT 1' &> /dev/null; then
 			break
 		fi
 		sleep 1
 	done
 	if [ "$i" = 0 ]; then
 		mariadb_error "Unable to start server."
+		exit 1;
 	fi
-
-	mariadb_note "Securing installation"
+	
+	mariadb_note "Setting root password"
 	MARIADB_ROOT_PASSWORD="$(< $MARIADB_ROOT_PASSWORD_FILE)"
-	mariadb-secure-installation <<-EOF
-
-		y
-		"$MARIADB_ROOT_PASSWORD"
-		"$MARIADB_ROOT_PASSWORD"
-		y
-		y
-		y
-		y
-	EOF
+	mariadb <<< "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD'"
 
 	mariadb_note "Creating database '$WORDPRESS_DB_NAME' and user '$WORDPRESS_DB_USER'"
 	MARIADB_WPUSER_PASSWORD="$(< $MARIADB_WPUSER_PASSWORD_FILE)"
-	mariadb -u root -p$MARIADB_ROOT_PASSWORD <<-EOF
+	mariadb <<-EOF
 		CREATE DATABASE $WORDPRESS_DB_NAME;
 		CREATE USER '$WORDPRESS_DB_USER'@'%' IDENTIFIED BY '$MARIADB_WPUSER_PASSWORD';
-		GRANT ALL PRIVILEGES ON *.* TO '$WORDPRESS_DB_USER'@'%' WITH GRANT OPTION;
+		GRANT ALL PRIVILEGES ON $WORDPRESS_DB_NAME.* TO '$WORDPRESS_DB_USER'@'%' WITH GRANT OPTION;
 		FLUSH PRIVILEGES;
 	EOF
 
 	mariadb_note "Shutdown server"
 	kill "$MARIADB_PID"
 	wait "$MARIADB_PID"
+
 	mariadb_note "Setup complete"
 fi
 
