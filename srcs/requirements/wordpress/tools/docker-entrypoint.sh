e776103af9f27cbeaf95afc3877b10eb2c60b80b
Gabriel Wolf <gwolf@student.42vienna.com>
2024-05-13 14:51:52 +0200

    feat: integrate env for wordpress admin and user

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -26,14 +26,29 @@ if ! wp core is-installed 2>/dev/null; then
 		--dbuser=${WORDPRESS_DB_USER} \
 		--dbpass=${MARIADB_WPUSER_PASSWORD} \
 		--dbhost=${MARIADB_HOST}
+	unset MARIADB_WPUSER_PASSWORD
 
 	wp-php_note "Install wordpress"
+	WORDPRESS_ADMIN_PASSWORD="$(< $WORDPRESS_ADMIN_PASSWORD_FILE)"
 	wp core install \
-		--url=localhost \
-		--title=inception \
-		--admin_user=admin \
-		--admin_password=admin \
-		--admin_email=admin@admin.com
+		--url=${DOMAIN_NAME} \
+		--title=${WORDPRESS_TITLE} \
+		--admin_user=${WORDPRESS_ADMIN} \
+		--admin_email="${WORDPRESS_ADMIN_MAIL}" \
+		--admin_password="${WORDPRESS_ADMIN_PASSWORD}" \
+		--skip-email
+	unset WORDPRESS_ADMIN_PASSWORD
+
+	if ! wp user get ${WORDPRESS_USER} 2>/dev/null; then
+		wp-php_note "Create second wordpress user"
+		WORDPRESS_USER_PASSWORD="$(< $WORDPRESS_USER_PASSWORD_FILE)"
+		wp user create \
+			${WORDPRESS_USER} \
+			"${WORDPRESS_USER_MAIL}" \
+			--role=author \
+			--user_pass=${WORDPRESS_USER_PASSWORD}
+		unset WORDPRESS_USER_PASSWORD
+	fi
 
 	wp theme install neve --activate
 else
