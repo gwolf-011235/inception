7d258b269ded6b86d85a412884e77b75347ebd2f
Gabriel Wolf <gwolf@student.42vienna.com>
2024-05-13 13:28:49 +0200

    fix: use db install and create mysql user

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -21,12 +21,16 @@ mariadb_note "Running mariadb docker-entrypoint"
 # Check if container is set up
 if [ ! -d /var/lib/mysql/$WORDPRESS_DB_NAME ]; then
 	mariadb_note "Container mariadb is not set up, setting up now"
+
+	mariadb_note "Initializing database files"
+	mariadb-install-db --rpm --datadir=/var/lib/mysql --auth-root-authentication-method=normal --skip-test-db
+
 	mariadb_note "Temp startup server"
 	mariadbd --skip-networking &
 	MARIADB_PID=$!
 	mariadb_note "Wait for server startup"
 	for i in {30..0}; do
-		if mariadb --database=mysql <<<'SELECT 1' &> /dev/null; then
+		if mariadb -u root --database=mysql <<<'SELECT 1' &> /dev/null; then
 			break
 		fi
 		sleep 1
@@ -35,9 +39,16 @@ if [ ! -d /var/lib/mysql/$WORDPRESS_DB_NAME ]; then
 		mariadb_error "Unable to start server."
 	fi
 	
+	mariadb_note "Create mysql user"
+	mariadb -u root <<-EOF
+		CREATE USER 'mysql'@'localhost' IDENTIFIED VIA unix_socket;
+		GRANT ALL PRIVILEGES ON *.* TO 'mysql'@'localhost' WITH GRANT OPTION;
+		FLUSH PRIVILEGES;
+	EOF
+
 	mariadb_note "Setting root password"
 	MARIADB_ROOT_PASSWORD="$(< $MARIADB_ROOT_PASSWORD_FILE)"
-	mariadb <<< "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD'"
+	mariadb -u root <<< "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD'"
 
 	mariadb_note "Creating database '$WORDPRESS_DB_NAME' and user '$WORDPRESS_DB_USER'"
 	MARIADB_WPUSER_PASSWORD="$(< $MARIADB_WPUSER_PASSWORD_FILE)"
