b9c3059c6baeea17b3f5afae9b4fc44265b4e00a
Gabriel Wolf <gwolf@student.42vienna.com>
2024-12-07 15:39:40 +0100

    feat: make ftp work

--- srcs/requirements/bonus/ftp/tools/docker-entrypoint.sh
+++ srcs/requirements/bonus/ftp/tools/docker-entrypoint.sh
@@ -13,15 +13,18 @@ ftp_note() {
 
 ftp_note "Running ftp docker-entrypoint"
 
-# Check if container is set up
-if [ ! -e index.php ] && [ ! -e wp-includes/version.php ]; then
+# Create home dir and update vsftpd user db:
+ftp_note "Setup FTP user"
+FTP_USER_PASSWORD="$(< $FTP_USER_PASSWORD_FILE)"
+echo -e "${FTP_USER}\n${FTP_USER_PASSWORD}" > /etc/vsftpd/virtual_users.txt
+/usr/bin/db_load -T -t hash -f /etc/vsftpd/virtual_users.txt /etc/vsftpd/virtual_users.db
+chmod 600 /etc/vsftpd/virtual_users.db
+rm /etc/vsftpd/virtual_users.txt
 
-	ftp_note "Setup FTP user"
-	FTP_USER_PASSWORD="$(< $FTP_USER_PASSWORD_FILE)"
-
-else
-	ftp_note "FTP user is setup."
-fi
+# Send logfile to stdout
+# touch /var/log/vsftpd/vsftpd.log
+# chown vsftpd:vsftpd /var/log/vsftpd/vsftpd.log
+# /usr/bin/ln -sf /dev/stdout /var/log/vsftpd/vsftpd.log
 
 ftp_note "Executing command '$@'"
 exec "$@"
