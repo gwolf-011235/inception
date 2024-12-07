8b6913e021527238ec8526bbb39bb4f654425af5
Gabriel Wolf <gwolf@student.42vienna.com>
2024-12-07 10:43:36 +0100

    feat: add password gen for ftp user

--- tools/create_passwords.sh
+++ tools/create_passwords.sh
@@ -2,17 +2,20 @@
 
 echo "Generating passwords."
 
-echo "1/5 Create password for MariaDB root."
+echo "1/6 Create password for MariaDB root."
 openssl rand -base64 20 > mariadb_root_password.txt
 
-echo "2/5 Create password for MariaDb user 'wpuser'."
+echo "2/6 Create password for MariaDb user 'wpuser'."
 openssl rand -base64 20 > mariadb_wpuser_password.txt
 
-echo "3/5 Create password for Wordpress admin."
+echo "3/6 Create password for Wordpress admin."
 openssl rand -base64 20 > wordpress_admin_password.txt
 
-echo "4/5 Create password for Wordpress user 'wpuser'."
+echo "4/6 Create password for Wordpress user 'wpuser'."
 openssl rand -base64 20 > wordpress_user_password.txt
 
-echo "5/5 Success: password files generated!"
+echo "5/6 Create password for FTP user."
+openssl rand -base64 20 > ftp_user_password.txt
+
+echo "6/6 Success: password files generated!"
 
