a52bc2472c6fbebb5d769546624a5a5c0d32ae49
Gabriel Wolf <gwolf@student.42vienna.com>
2025-02-01 08:48:45 +0100

    fix: set guest_username in entry script

--- srcs/requirements/bonus/ftp/tools/docker-entrypoint.sh
+++ srcs/requirements/bonus/ftp/tools/docker-entrypoint.sh
@@ -16,17 +16,25 @@ ftp_note "Running ftp docker-entrypoint"
 # Check if container is set up
 if [ ! -e /etc/vsftpd/init ]; then
 	ftp_note "Container ftp is not set up, setting up now"
+
 	ftp_note "Update vsftpd user db with ${USER_NAME}"
 	FTP_USER_PASSWORD="$(< $FTP_USER_PASSWORD_FILE)"
 	echo -e "${USER_NAME}\n${FTP_USER_PASSWORD}" > /etc/vsftpd/virtual_users.txt
 	db_load -T -t hash -f /etc/vsftpd/virtual_users.txt /etc/vsftpd/virtual_users.db
 	chmod 600 /etc/vsftpd/virtual_users.db
 	rm /etc/vsftpd/virtual_users.txt
-	touch /etc/vsftpd/init
+
+	ftp_note "Chown the home directory"
+	chown -R ${USER_NAME}:${USER_NAME} /home/${USER_NAME}
+	
+	ftp_note "Updating config file with ${USER_NAME}"
+	echo "guest_username=${USER_NAME}" >> /etc/vsftpd/vsftpd.conf
 
 	ftp_note "Send logfile to stdout"
 	touch /var/log/vsftpd/vsftpd.log
 	tail -f /var/log/vsftpd/vsftpd.log &
+
+	touch /etc/vsftpd/init
 fi
 
 ftp_note "Executing command '$@'"
